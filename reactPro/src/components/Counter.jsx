ec77e93eba1700038fa5a9e6c7089a29fc79ae86
Sebghatullah Amin <95332899+SebghatAmin@users.noreply.github.com>
2026-09-05 16:45:30 +0430

    updated

--- reactPro/src/components/Counter.jsx
+++ reactPro/src/components/Counter.jsx
@@ -1,23 +1,43 @@
 import React from 'react'
 export default function Counter(){
     const [count, setCount] = React.useState(0)
+    const [incrementBy,setIncrementBy]=React.useState(1);
     function increamint(){
-        setCount(count + 1)
+        setCount(count + incrementBy)
     }
     function decreamint(){
-        setCount(count - 1)
+        setCount(count - incrementBy)
     }
     function reset(){
         setCount(0)
     }
+    function increaseincreament(){
+        
+        setIncrementBy(incrementBy+1)
+    }
+    function decreaseincreament(){
+        
+        setIncrementBy(incrementBy-1)
+    }
+    
     return (
 
         <div>
-    <h1>Your value is:{count}</h1>
-
+            <h1>
+                Your value is:{count}
+            </h1>
             <button onClick={increamint}>+</button>
             <button onClick={decreamint}>-</button>
+                        <h1>increased by {incrementBy}</h1>
+
+            <button onClick={increaseincreament}>increasBy</button>
+            <h1>decreasedBy{incrementBy}</h1>
+            <button onClick={decreaseincreament}>decreaseBy</button>
+            <br />
+            <h1>reset bellow</h1>
             <button onClick={reset}>Reset</button>
+
+
         </div>
     )
 }
